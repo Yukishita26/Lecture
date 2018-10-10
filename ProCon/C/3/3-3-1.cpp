8280b849199fb1a8c295d93d5711b974f5b23060
Yuki_Sakishita <sakkii51@hotmail.co.jp>
2018-10-10 23:21:07 +0900

    わからん

--- ProCon/C/3/3-3-1.cpp
+++ ProCon/C/3/3-3-1.cpp
@@ -1,16 +1,19 @@
+#if 0
+1つの発電機で通電する区域が発電機の数だけできる．
+この区域でない部分，つまり家と家の間(n-1)箇所の和を最大化すれば送電線の長さを最小化できる．
+#endif
 #include <algorithm>
 #include <iostream>
 #define int long long
 using namespace std;
 int t, n, k, x[100000], d[99999];
 int solve(){
-    if(k>n)k=n;
     for(int i=0; i<n-1; i++){
         d[i] = x[i+1] - x[i];
     }
     sort(d,d+n-1);
     int w = 0;
-    for(int i=0; i<k-1; i++){
+    for(int i=0; i<k-1 && i<n-1; i++){
         w += d[n-2-i];
     }
     return x[n-1] - x[0] - w;
--- ProCon/C/3/3-3-3.cpp
+++ ProCon/C/3/3-3-3.cpp
@@ -1,15 +1,41 @@
+#if 0
+サンプルケースは通りましたがなぜテストに通らないのかがわかりません．
+考え方は
+ xの小さい島から見ていき，そこにレーダーが届く設置場所の範囲と共通部分を持つものを貪欲法で集めていく．
+ このとき範囲の最右点だけ見ていれば，共通部分の最右点はそのまま変わらないか，xと最右点の間になるため追跡できる．
+ 範囲と共通点を持たないものがあれば新たにレーダーを設置すれば良い．
+としましたがWrongAnswerが出てしまいます．
+どうにもわからないのでひとまず提出します．
+#endif
 #include <algorithm>
 #include <iostream>
 #include <utility>
+#include <math.h>
 #define int long long
 using namespace std;
 int n, d;
 pair<int, int> xy[1000];
-//bool com(pair<int,int> a, pair<int,int> b){ //Pの値で降順ソート
-//    return a.second > b.second;
-//}
+double ld(int d, int y){
+    return sqrt((double)d*d-y*y);
+}
 int solve(){
-    
+    sort(xy,xy+n);
+    int r=1, x=xy[0].first, y=xy[0].second;
+    for(int i=1; i<n; i++){
+        int xi = xy[i].first, yi = xy[i].second;
+        if(yi > d)return -1;
+        if(x+ld(d,y) >= xi-ld(d,yi)){//現在の範囲と共有部分がある (   x { ) z    }
+            if(x+ld(d,y) > xi+ld(d,yi)){//範囲が狭くなる (      x  { z })
+                x = xi;
+                y = yi;//範囲を更新
+            }
+        }else{ //共通部分がない (    x    ) {  z  }
+            r++;//新たなRader
+            x = xi;
+            y = yi;
+        }
+    }
+
     return r;
 }
 bool get(){
@@ -19,9 +45,12 @@ bool get(){
     for(int i=0; i<n; i++){
         cin >> xy[i].first >> xy[i].second;
     }
+    return true;
 }
 signed main(){
+    int cs = 1;
     while(get()){
-        cout << solve() << endl;
+        cout << "Case " << cs << ": " << solve() << endl;
+        cs++;
     }
 }
